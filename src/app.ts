ecd85aaca38a8352ef44cbfb1b4ff2769a880914
Adriana <adriana.silva.silveira@gmail.com>
2024-05-01 14:29:12 -0300

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express, { json } from "express";
-import helmet from "helmet";
+import "reflect-metadata";
 import "express-async-errors";
+import cors from "cors";
+import helmet from "helmet";
 import { HandleErrors } from "./middlewares/handleErrors.middleware";
+import { userRouter } from "./routes/userRoutes";
+import express, { json } from "express";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(helmet());
 
 app.use(json());
+app.use("/users", userRouter);
 
 app.use(HandleErrors.execute);
--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -6,3 +6,12 @@ export const userSchema = z.object({
    email: z.string().min(1).email(),
    password: z.string().min(8)
 });
+
+export const CreateUserSchema = userSchema.omit({id:true});
+export const LoginUserSchema = userSchema.pick({email: true, password: true});
+export const UserReturnSchema = userSchema.omit({password: true})
+
+// export const TokenSchema = z.object({token: z.string()})
+// export const BodyGetTasksSchema = TasksSchema.omit({categoryId: true}).extend({category: GetCategoryList});
+
+export const ReturnLoginSchema =  UserReturnSchema.extend({token: z.string()});
